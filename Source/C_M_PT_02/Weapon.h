7f09845fe532f674e80bfc6d27a0a54c0550eb8e
Vortex <bulkasmi@gmail.com>
2021-11-08 14:43:26 +0200

    Add weapon component to the player character (intermediate commit)

---
+++ Source/C_M_PT_02/Weapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class C_M_PT_02_API UWeapon : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
